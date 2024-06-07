c682fc4a5e28f543707fab98903f0207c025b078
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-07 16:40:29 -0300

    feat: Criar entidade category (Categoria)
    Issue: #1

---
+++ src/main/java/com/controlcash/app/models/Category.java
@@ -0,0 +1,43 @@
+package com.controlcash.app.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Goal> goals;
+
+    public Category() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Goal> getGoals() {
+        return this.goals;
+    }
+}
