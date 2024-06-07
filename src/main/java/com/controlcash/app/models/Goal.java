57b418cb18dd766dcf9c6d626b125ca7dbb35cf1
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-07 16:24:52 -0300

    feat: Criar entitidade Goal (Objetivo)
    Issue: #1

---
+++ src/main/java/com/controlcash/app/models/Goal.java
@@ -0,0 +1,46 @@
+package com.controlcash.app.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class Goal implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Date dueDate;
+    private Double value;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private User user;
+
+    public Goal() {}
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public Date getDueDate() {
+        return this.dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Double getValue() {
+        return this.value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
