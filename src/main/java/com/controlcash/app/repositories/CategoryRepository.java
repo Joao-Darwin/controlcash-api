688243f07b97bed420dd990f063621e112da1dd8
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-10 15:55:08 -0300

    feat: Implementar repository da entidade Category
    Issue: #2

---
+++ src/main/java/com/controlcash/app/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.controlcash.app.repositories;
+
+import com.controlcash.app.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
