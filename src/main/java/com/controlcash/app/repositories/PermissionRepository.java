6456dbe99f00c8ad2c9f2c48decdb405c3cf8928
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-10 15:54:41 -0300

    feat: Implementar repository da entidade Permission
    Issue: #2

---
+++ src/main/java/com/controlcash/app/repositories/PermissionRepository.java
@@ -0,0 +1,11 @@
+package com.controlcash.app.repositories;
+
+import com.controlcash.app.models.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, UUID> {
+}
