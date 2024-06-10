7eb5a86771f026bfc2a7855afecf9d5824715af0
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-10 15:53:53 -0300

    feat: Implementar repository da entidade Transaction
    Issue: #2

---
+++ src/main/java/com/controlcash/app/repositories/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.controlcash.app.repositories;
+
+import com.controlcash.app.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+}
