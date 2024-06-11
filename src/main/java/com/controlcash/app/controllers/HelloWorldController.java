4fcacee8ce0af83cd0f4d3066a8dbde0af03fb47
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-11 16:44:15 -0300

    revert: Reverter commit que altera retorno do endpoint de hello world

--- src/main/java/com/controlcash/app/controllers/HelloWorldController.java
+++ src/main/java/com/controlcash/app/controllers/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.controlcash.app.controllers;
 
-import com.controlcash.app.exceptions.ResponseEntityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-
 @RestController
 @RequestMapping(value = "${path-api}/helloWorld")
 public class HelloWorldController {
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<?> helloWorld() {
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseEntityException(Instant.now(), "", ""));
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World");
     }
 }
