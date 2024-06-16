da0c43e435ac85e92ffa09757808fb031ce424fc
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-16 00:19:15 -0300

    feat: Adicionar método de create no service de User
    Issue: #3

--- src/main/java/com/controlcash/app/services/UserService.java
+++ src/main/java/com/controlcash/app/services/UserService.java
@@ -1,6 +1,10 @@
 package com.controlcash.app.services;
 
+import com.controlcash.app.dtos.request.UserCreateRequestDTO;
+import com.controlcash.app.dtos.response.UserCreateResponseDTO;
+import com.controlcash.app.models.User;
 import com.controlcash.app.repositories.UserRepository;
+import com.controlcash.app.utils.converters.UserConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +16,14 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public UserCreateResponseDTO create(UserCreateRequestDTO userCreateRequestDTO) {
+        User user = UserConverter.convertUserCreateRequestDTOToUser(userCreateRequestDTO);
+
+        // TODO: Add password encrypt before save on database
+        user = userRepository.save(user);
+
+        return UserConverter.convertUserToUserCreateResponseDTO(user);
     }
 }
