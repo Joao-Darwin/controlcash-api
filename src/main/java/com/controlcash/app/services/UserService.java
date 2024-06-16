42074d77d0010b7c4cad90526a240fc4be45f89c
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-15 22:45:56 -0300

    feat: Adicionar injeção de depedência do repository a classe de service do usuário
    Issue: #3

--- src/main/java/com/controlcash/app/services/UserService.java
+++ src/main/java/com/controlcash/app/services/UserService.java
@@ -1,4 +1,16 @@
 package com.controlcash.app.services;
 
+import com.controlcash.app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
