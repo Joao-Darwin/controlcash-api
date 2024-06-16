b4462f5691bcd4d59fd0f8e2c21a6edb71b6d34b
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-16 18:48:10 -0300

    feat: Criar método de findAll no service de usuário
    Issue: #3

--- src/main/java/com/controlcash/app/services/UserService.java
+++ src/main/java/com/controlcash/app/services/UserService.java
@@ -1,12 +1,17 @@
 package com.controlcash.app.services;
 
 import com.controlcash.app.dtos.request.UserCreateRequestDTO;
+import com.controlcash.app.dtos.response.UserAllResponseDTO;
 import com.controlcash.app.dtos.response.UserCreateResponseDTO;
 import com.controlcash.app.models.User;
 import com.controlcash.app.repositories.UserRepository;
 import com.controlcash.app.utils.converters.UserConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -25,5 +30,11 @@ public class UserService {
         user = userRepository.save(user);
 
         return UserConverter.convertUserToUserCreateResponseDTO(user);
+    }
+
+    public List<UserAllResponseDTO> findAll(Pageable pageable) {
+        Page<User> users = userRepository.findAll(pageable);
+
+        return users.map(UserConverter::convertUserToUserAllResponseDTO).stream().toList();
     }
 }
