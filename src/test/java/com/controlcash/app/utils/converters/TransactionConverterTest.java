3f53cbac6af085beac7ef0d5468b0b8062ca485a
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-23 01:02:03 -0300

    test: Adicionar testes de conversão de Transaction para DTO
    Issue: #3

--- src/test/java/com/controlcash/app/utils/converters/TransactionConverterTest.java
+++ src/test/java/com/controlcash/app/utils/converters/TransactionConverterTest.java
@@ -1,13 +1,16 @@
 package com.controlcash.app.utils.converters;
 
 import com.controlcash.app.dtos.request.TransactionCreateRequestDTO;
+import com.controlcash.app.dtos.response.TransactionCreateResponseDTO;
 import com.controlcash.app.models.Category;
 import com.controlcash.app.models.Transaction;
 import com.controlcash.app.models.enums.TransactionType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class TransactionConverterTest {
 
@@ -27,5 +30,24 @@ public class TransactionConverterTest {
         Assertions.assertEquals(expectedValue, transaction.getValue());
         Assertions.assertEquals(expectedAmountRepeat, transaction.getAmountRepeat());
         Assertions.assertEquals(expectedTransactionType, transaction.getTransactionType());
+    }
+
+    @Test
+    public void testConverterTransactionToTransactionCreateResponseDTO_ShouldReturnATransactionCreateResponseDTO() {
+        UUID expectedId = UUID.randomUUID();
+        String expectedName = "Month Salary";
+        String expectedDescription = "my month salary from job";
+        Date expectedCreatedDate = new Date();
+        double expectedValue = 1600.00;
+        int expectedAmountRepeat = 1;
+        TransactionType expectedTransactionType = TransactionType.ENTRANCE;
+        Transaction transaction = new Transaction(expectedId, expectedName, expectedDescription, expectedCreatedDate, expectedValue, expectedAmountRepeat, expectedTransactionType);
+
+        TransactionCreateResponseDTO transactionCreateResponseDTO = TransactionConverter.convertTransactionToTransactionCreateResponseDTO(transaction);
+
+        Assertions.assertEquals(expectedId, transactionCreateResponseDTO.id());
+        Assertions.assertEquals(expectedName, transactionCreateResponseDTO.name());
+        Assertions.assertEquals(expectedDescription, transactionCreateResponseDTO.description());
+        Assertions.assertEquals(expectedCreatedDate, transactionCreateResponseDTO.createdDate());
     }
 }
