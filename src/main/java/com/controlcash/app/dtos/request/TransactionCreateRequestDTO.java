f3a1b4d06673de4d4984cda9272cdc5f4b23332b
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-23 00:28:06 -0300

    feat: Adicionar lista de categorias ao dto de request de transaction
    Issue: #3

--- src/main/java/com/controlcash/app/dtos/request/TransactionCreateRequestDTO.java
+++ src/main/java/com/controlcash/app/dtos/request/TransactionCreateRequestDTO.java
@@ -1,6 +1,9 @@
 package com.controlcash.app.dtos.request;
 
+import com.controlcash.app.models.Category;
 import com.controlcash.app.models.enums.TransactionType;
 
-public record TransactionCreateRequestDTO(String name, String description, Double value, Integer amountRepeat, TransactionType transactionType) {
+import java.util.List;
+
+public record TransactionCreateRequestDTO(String name, String description, Double value, Integer amountRepeat, TransactionType transactionType, List<Category> categories) {
 }
