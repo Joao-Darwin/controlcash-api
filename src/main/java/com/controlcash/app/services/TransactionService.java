32ecfd4a824d1e23f7844649c21ccdad091f705a
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-23 01:05:18 -0300

    feat: Adicionar create de transaction no seu service
    Issue: #3

--- src/main/java/com/controlcash/app/services/TransactionService.java
+++ src/main/java/com/controlcash/app/services/TransactionService.java
@@ -1,6 +1,10 @@
 package com.controlcash.app.services;
 
+import com.controlcash.app.dtos.request.TransactionCreateRequestDTO;
+import com.controlcash.app.dtos.response.TransactionCreateResponseDTO;
+import com.controlcash.app.models.Transaction;
 import com.controlcash.app.repositories.TransactionRepository;
+import com.controlcash.app.utils.converters.TransactionConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +18,11 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
     }
 
-    
+    public TransactionCreateResponseDTO create(TransactionCreateRequestDTO transactionCreateRequestDTO) {
+        Transaction transaction = TransactionConverter.convertTransactionCreateRequestDTOToTransaction(transactionCreateRequestDTO);
+
+        transaction = transactionRepository.save(transaction);
+
+        return TransactionConverter.convertTransactionToTransactionCreateResponseDTO(transaction);
+    }
 }
