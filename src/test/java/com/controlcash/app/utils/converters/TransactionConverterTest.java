0267959c43851d44cdd3dc9b3f974a1c082eb0d5
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-23 22:32:58 -0300

    test: Criar testes para método utulitário de conversão de Transaction para TransactionCompleteResponseDTO
    Issue: #3

--- src/test/java/com/controlcash/app/utils/converters/TransactionConverterTest.java
+++ src/test/java/com/controlcash/app/utils/converters/TransactionConverterTest.java
@@ -2,6 +2,7 @@ package com.controlcash.app.utils.converters;
 
 import com.controlcash.app.builder.TransactionBuilder;
 import com.controlcash.app.dtos.request.TransactionCreateRequestDTO;
+import com.controlcash.app.dtos.response.TransactionCompleteResponseDTO;
 import com.controlcash.app.dtos.response.TransactionCreateResponseDTO;
 import com.controlcash.app.models.Category;
 import com.controlcash.app.models.Transaction;
@@ -57,5 +58,34 @@ public class TransactionConverterTest {
         Assertions.assertEquals(expectedName, transactionCreateResponseDTO.name());
         Assertions.assertEquals(expectedDescription, transactionCreateResponseDTO.description());
         Assertions.assertEquals(expectedCreatedDate, transactionCreateResponseDTO.createdDate());
+    }
+
+    @Test
+    public void testConverterTransactionToTransactionCompleteResponseDTO_ShouldReturnATransactionCompleteResponseDTO() {
+        UUID expectedId = UUID.randomUUID();
+        String expectedName = "Month Salary";
+        String expectedDescription = "my month salary from job";
+        Date expectedCreatedDate = new Date();
+        double expectedValue = 1600.00;
+        int expectedAmountRepeat = 1;
+        TransactionType expectedTransactionType = TransactionType.ENTRANCE;
+        Transaction transaction = new TransactionBuilder(expectedTransactionType)
+                .addId(expectedId)
+                .addName(expectedName)
+                .addDescription(expectedDescription)
+                .addCreatedDate(expectedCreatedDate)
+                .addValue(expectedValue)
+                .addAmountRepeat(expectedAmountRepeat)
+                .build();
+
+        TransactionCompleteResponseDTO transactionCompleteResponseDTO = TransactionConverter.convertTransactionToTransactionCompleteResponseDTO(transaction);
+
+        Assertions.assertEquals(expectedId, transactionCompleteResponseDTO.id());
+        Assertions.assertEquals(expectedName, transactionCompleteResponseDTO.name());
+        Assertions.assertEquals(expectedDescription, transactionCompleteResponseDTO.description());
+        Assertions.assertEquals(expectedCreatedDate, transactionCompleteResponseDTO.createdDate());
+        Assertions.assertEquals(expectedValue, transaction.getValue());
+        Assertions.assertEquals(expectedAmountRepeat, transaction.getAmountRepeat());
+        Assertions.assertEquals(expectedTransactionType, transaction.getTransactionType());
     }
 }
