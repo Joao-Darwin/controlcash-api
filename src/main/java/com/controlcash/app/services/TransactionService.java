56bf6d5147ca0b50c969a038fcc0de6fb07269f5
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-23 22:19:14 -0300

    feat: Criar método de findAll no service de Transaction
    Issue: #3

--- src/main/java/com/controlcash/app/services/TransactionService.java
+++ src/main/java/com/controlcash/app/services/TransactionService.java
@@ -6,7 +6,11 @@ import com.controlcash.app.models.Transaction;
 import com.controlcash.app.repositories.TransactionRepository;
 import com.controlcash.app.utils.converters.TransactionConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
@@ -24,5 +28,11 @@ public class TransactionService {
         transaction = transactionRepository.save(transaction);
 
         return TransactionConverter.convertTransactionToTransactionCreateResponseDTO(transaction);
+    }
+
+    public List<TransactionCreateResponseDTO> findAll(Pageable pageable) {
+        Page<Transaction> transactions = transactionRepository.findAll(pageable);
+
+        return transactions.map(TransactionConverter::convertTransactionToTransactionCreateResponseDTO).toList();
     }
 }
