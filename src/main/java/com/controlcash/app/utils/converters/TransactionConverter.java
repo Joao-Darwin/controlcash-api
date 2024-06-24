d8bb356045959cd676ebd5b275ccacac481d566b
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-23 22:34:42 -0300

    feat: Criar método utilitário de conversão de Transaction para TransactionCompleteResponseDTO
    Issue: #3

--- src/main/java/com/controlcash/app/utils/converters/TransactionConverter.java
+++ src/main/java/com/controlcash/app/utils/converters/TransactionConverter.java
@@ -2,6 +2,7 @@ package com.controlcash.app.utils.converters;
 
 import com.controlcash.app.builder.TransactionBuilder;
 import com.controlcash.app.dtos.request.TransactionCreateRequestDTO;
+import com.controlcash.app.dtos.response.TransactionCompleteResponseDTO;
 import com.controlcash.app.dtos.response.TransactionCreateResponseDTO;
 import com.controlcash.app.models.Transaction;
 
@@ -20,5 +21,16 @@ public class TransactionConverter {
 
     public static TransactionCreateResponseDTO convertTransactionToTransactionCreateResponseDTO(Transaction transaction) {
         return new TransactionCreateResponseDTO(transaction.getId(), transaction.getName(), transaction.getDescription(), transaction.getCreatedDate());
+    }
+
+    public static TransactionCompleteResponseDTO convertTransactionToTransactionCompleteResponseDTO(Transaction transaction) {
+        return new TransactionCompleteResponseDTO(transaction.getId(),
+                transaction.getName(),
+                transaction.getDescription(),
+                transaction.getCreatedDate(),
+                transaction.getValue(),
+                transaction.getAmountRepeat(),
+                transaction.getTransactionType(),
+                transaction.getCategories());
     }
 }
