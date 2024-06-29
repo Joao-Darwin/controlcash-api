05e15eb34f9ead8173d6517c51113dd1138df8a0
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-29 00:29:49 -0300

    test: Criar testes para método utilitário que converte Goal para GoalCompleteResponseDTO
    Issue: #3

--- src/test/java/com/controlcash/app/utils/converters/GoalConverterTest.java
+++ src/test/java/com/controlcash/app/utils/converters/GoalConverterTest.java
@@ -1,6 +1,7 @@
 package com.controlcash.app.utils.converters;
 
 import com.controlcash.app.dtos.goal.request.GoalCreateRequestDTO;
+import com.controlcash.app.dtos.goal.response.GoalCompleteResponseDTO;
 import com.controlcash.app.models.Category;
 import com.controlcash.app.models.Goal;
 import com.controlcash.app.models.User;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.TestInstance;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.UUID;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GoalConverterTest {
@@ -29,5 +31,23 @@ public class GoalConverterTest {
         Assertions.assertEquals(expectedGoalCreateRequestDTO.value(), actualGoal.getValue());
         Assertions.assertEquals(expectedGoalCreateRequestDTO.user(), actualGoal.getUser());
         Assertions.assertEquals(expectedGoalCreateRequestDTO.category(), actualGoal.getCategory());
+    }
+
+    @Test
+    public void testConvertGoalToGoalCompleteResponseDTO_ShouldReturnGoalCompleteResponseDTO() {
+        UUID expectedId = UUID.randomUUID();
+        Date expectedDueDate = Date.from(Instant.now());
+        Double expectedValue = 1250.00;
+        User expectedUser = new User();
+        Category expectedCategory = new Category();
+        Goal goal = new Goal(expectedId, expectedDueDate, expectedValue, expectedUser, expectedCategory);
+
+        GoalCompleteResponseDTO goalCompleteResponseDTO = GoalConverter.convertGoalToGoalCompleteResponseDTO(goal);
+
+        Assertions.assertEquals(expectedId, goalCompleteResponseDTO.id());
+        Assertions.assertEquals(expectedDueDate, goalCompleteResponseDTO.dueDate());
+        Assertions.assertEquals(expectedValue, goalCompleteResponseDTO.value());
+        Assertions.assertEquals(expectedUser, goalCompleteResponseDTO.user());
+        Assertions.assertEquals(expectedCategory, goalCompleteResponseDTO.category());
     }
 }
