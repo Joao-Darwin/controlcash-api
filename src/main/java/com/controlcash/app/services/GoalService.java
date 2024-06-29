89a9ab5588917c5489a0d3bf1aca6621758db1ed
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-29 01:04:50 -0300

    feat: Implementar findAll no service da entity Goal
    Issue: #3

--- src/main/java/com/controlcash/app/services/GoalService.java
+++ src/main/java/com/controlcash/app/services/GoalService.java
@@ -2,10 +2,13 @@ package com.controlcash.app.services;
 
 import com.controlcash.app.dtos.goal.request.GoalCreateRequestDTO;
 import com.controlcash.app.dtos.goal.response.GoalCompleteResponseDTO;
+import com.controlcash.app.dtos.goal.response.GoalSimpleResponseDTO;
 import com.controlcash.app.models.Goal;
 import com.controlcash.app.repositories.GoalRepository;
 import com.controlcash.app.utils.converters.GoalConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,5 +27,11 @@ public class GoalService {
         goal = goalRepository.save(goal);
 
         return GoalConverter.convertGoalToGoalCompleteResponseDTO(goal);
+    }
+
+    public Page<GoalSimpleResponseDTO> findAll(Pageable pageable) {
+        Page<Goal> goals = goalRepository.findAll(pageable);
+
+        return goals.map(GoalConverter::convertGoalToGoalSimpleResponseDTO);
     }
 }
