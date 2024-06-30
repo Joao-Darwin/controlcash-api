ba8b1bbbd7292a7331e29d7e89859f7312dc366b
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-29 21:55:06 -0300

    feat: Criar service de Category
    Issue: #3

---
+++ src/main/java/com/controlcash/app/services/CategoryService.java
@@ -0,0 +1,16 @@
+package com.controlcash.app.services;
+
+import com.controlcash.app.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+}
