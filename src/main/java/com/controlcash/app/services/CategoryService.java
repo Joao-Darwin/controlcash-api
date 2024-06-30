a737f88cded9a40b649253c328091436c7606a65
João Darwin <joaodarwin.ip22@gmail.com>
2024-06-29 22:46:35 -0300

    feat: Implementar create no service de Category
    Issue: #3

--- src/main/java/com/controlcash/app/services/CategoryService.java
+++ src/main/java/com/controlcash/app/services/CategoryService.java
@@ -1,6 +1,10 @@
 package com.controlcash.app.services;
 
+import com.controlcash.app.dtos.category.request.CategoryRequestDTO;
+import com.controlcash.app.dtos.category.response.CategoryResponseDTO;
+import com.controlcash.app.models.Category;
 import com.controlcash.app.repositories.CategoryRepository;
+import com.controlcash.app.utils.converters.CategoryConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +16,13 @@ public class CategoryService {
     @Autowired
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    public CategoryResponseDTO create(CategoryRequestDTO categoryRequestDTO) {
+        Category category = CategoryConverter.convertCategoryRequestDTOToCategory(categoryRequestDTO);
+
+        category = categoryRepository.save(category);
+
+        return CategoryConverter.convertCategoryToCategoryResponseDTO(category);
     }
 }
