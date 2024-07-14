44f0ddc82902bb7abb9c1baaaea40eb7db8a3ecc
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-13 23:27:35 -0300

    feat: Criar método de busca por todos as permission no service de permission
    Issue: #21

--- src/main/java/com/controlcash/app/services/PermissionService.java
+++ src/main/java/com/controlcash/app/services/PermissionService.java
@@ -1,12 +1,17 @@
 package com.controlcash.app.services;
 
 import com.controlcash.app.dtos.permission.request.PermissionCreateRequestDTO;
+import com.controlcash.app.dtos.permission.response.AllPermissionResponseDTO;
 import com.controlcash.app.dtos.permission.response.PermissionResponseDTO;
 import com.controlcash.app.models.Permission;
 import com.controlcash.app.repositories.PermissionRepository;
 import com.controlcash.app.utils.converters.PermissionConverter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PermissionService {
@@ -24,6 +29,12 @@ public class PermissionService {
         permission = permissionRepository.save(permission);
 
         return PermissionConverter.convertPermissionToPermissionResponseDTO(permission);
+    }
+
+    public Page<AllPermissionResponseDTO> findAll(Pageable pageable) {
+        Page<Permission> permissionPage = permissionRepository.findAll(pageable);
+
+        return permissionPage.map(PermissionConverter::convertPermissionToAllPermissionResponseDTO);
     }
 
 }
