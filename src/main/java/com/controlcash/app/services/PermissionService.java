58ce6a333dc7f39e43354cc5259250a89e9126ee
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-13 23:10:44 -0300

    feat: Implementar método de create no service de Permission
    Issue: #21

--- src/main/java/com/controlcash/app/services/PermissionService.java
+++ src/main/java/com/controlcash/app/services/PermissionService.java
@@ -1,18 +1,29 @@
 package com.controlcash.app.services;
 
+import com.controlcash.app.dtos.permission.request.PermissionCreateRequestDTO;
+import com.controlcash.app.dtos.permission.response.PermissionResponseDTO;
+import com.controlcash.app.models.Permission;
 import com.controlcash.app.repositories.PermissionRepository;
+import com.controlcash.app.utils.converters.PermissionConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PermissionService {
 
-    private PermissionRepository permissionRepository;
+    private final PermissionRepository permissionRepository;
 
     @Autowired
     public PermissionService(PermissionRepository permissionRepository) {
         this.permissionRepository = permissionRepository;
     }
 
+    public PermissionResponseDTO create(PermissionCreateRequestDTO permissionCreateRequestDTO) {
+        Permission permission = PermissionConverter.convertPermissionCreateRequestDTOToPermission(permissionCreateRequestDTO);
+
+        permission = permissionRepository.save(permission);
+
+        return PermissionConverter.convertPermissionToPermissionResponseDTO(permission);
+    }
 
 }
