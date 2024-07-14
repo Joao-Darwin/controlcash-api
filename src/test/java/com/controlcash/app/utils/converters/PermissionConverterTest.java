2528352d9f71417587e27c2d0c48415d251084a3
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-13 23:09:51 -0300

    test: Criar testes para conversão de Permission para DTO de response
    Issue: #21

--- src/test/java/com/controlcash/app/utils/converters/PermissionConverterTest.java
+++ src/test/java/com/controlcash/app/utils/converters/PermissionConverterTest.java
@@ -1,10 +1,16 @@
 package com.controlcash.app.utils.converters;
 
 import com.controlcash.app.dtos.permission.request.PermissionCreateRequestDTO;
+import com.controlcash.app.dtos.permission.response.PermissionResponseDTO;
 import com.controlcash.app.models.Permission;
+import com.controlcash.app.models.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.UUID;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PermissionConverterTest {
@@ -19,5 +25,22 @@ public class PermissionConverterTest {
         Assertions.assertNull(permission.getId());
         Assertions.assertNull(permission.getUsers());
         Assertions.assertEquals(expectedDescriptionPermission, permission.getAuthority());
+    }
+
+    @Test
+    void testConvertPermissionToPermissionResponseDTO_ShouldReturnAPermissionResponseDTO() {
+        List<User> userList = List.of();
+        UUID expectedId = UUID.randomUUID();
+        String expectedDescription = "ADMIN";
+        Permission permission = Mockito.mock(Permission.class);
+        Mockito.when(permission.getId()).thenReturn(expectedId);
+        Mockito.when(permission.getAuthority()).thenReturn(expectedDescription);
+        Mockito.when(permission.getUsers()).thenReturn(userList);
+
+        PermissionResponseDTO permissionResponseDTO = PermissionConverter.convertPermissionToPermissionResponseDTO(permission);
+
+        Assertions.assertEquals(expectedId, permissionResponseDTO.id());
+        Assertions.assertEquals(expectedDescription, permissionResponseDTO.description());
+        Assertions.assertEquals(expectedId, permissionResponseDTO.id());
     }
 }
