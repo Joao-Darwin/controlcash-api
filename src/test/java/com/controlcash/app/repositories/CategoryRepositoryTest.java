5df3a6cca36d9ca3049899a3b937c5c7e1e3eb39
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-18 22:54:55 -0300

    style: Substituir import card por importação manual
    Issue: #24

--- src/test/java/com/controlcash/app/repositories/CategoryRepositoryTest.java
+++ src/test/java/com/controlcash/app/repositories/CategoryRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.controlcash.app.repositories;
 
 import com.controlcash.app.models.Category;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
