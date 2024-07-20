8bb6c3a6bbbf0f1d13f6e8fda040531f4062af7c
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-19 23:19:54 -0300

    test: Implementar teste de save de goal sem user e category
    Issue: #25

--- src/test/java/com/controlcash/app/repositories/GoalRepositoryTest.java
+++ src/test/java/com/controlcash/app/repositories/GoalRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.controlcash.app.repositories;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.controlcash.app.models.Goal;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @DataJpaTest
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -12,5 +15,25 @@ public class GoalRepositoryTest {
     @Autowired
     private GoalRepository goalRepository;
 
+    private Goal goal;
 
+    @BeforeEach
+    void setUp() throws ParseException {
+        goal = new Goal();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        goal.setDueDate(dateFormat.parse("19/07/2024"));
+        goal.setValue(1500.00);
+    }
+
+    @Test
+    void testSave_GivenAGoalWithoutUserAndCategory_ShouldSaveAndReturnAGoal() {
+        Goal actualGoal = goalRepository.save(goal);
+
+        Assertions.assertNotNull(actualGoal);
+        Assertions.assertNotNull(actualGoal.getId());
+        Assertions.assertEquals(goal.getValue(), actualGoal.getValue());
+        Assertions.assertEquals(goal.getDueDate(), actualGoal.getDueDate());
+        Assertions.assertNull(actualGoal.getUser());
+        Assertions.assertNull(actualGoal.getCategory());
+    }
 }
