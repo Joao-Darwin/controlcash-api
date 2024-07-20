3e3bd588bbbfd0573bfcd31721964c46f6cdf74f
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-19 23:42:18 -0300

    test: Implementar testes para save de goal com todos os atributos
    Issue: #25

--- src/test/java/com/controlcash/app/repositories/GoalRepositoryTest.java
+++ src/test/java/com/controlcash/app/repositories/GoalRepositoryTest.java
@@ -1,7 +1,12 @@
 package com.controlcash.app.repositories;
 
 import com.controlcash.app.models.Goal;
-import org.junit.jupiter.api.*;
+import com.controlcash.app.models.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -24,6 +29,25 @@ public class GoalRepositoryTest {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         goal.setDueDate(dateFormat.parse("19/07/2024"));
         goal.setValue(1500.00);
+    }
+
+    @Test
+    void testSave_GivenAGoalWithAllAttributes_ShouldSaveAndReturnAGoal() {
+        User user = new User();
+        user.setUserName("user123");
+        user.setEmail("user123@gmail.com");
+        user.setFullName("User");
+        user.setSalary(1500.00);
+        user.setPassword("password");
+        goal.setUser(user);
+
+        Goal actualGoal = goalRepository.save(goal);
+
+        Assertions.assertNotNull(actualGoal);
+        Assertions.assertNotNull(actualGoal.getId());
+        Assertions.assertEquals(goal.getValue(), actualGoal.getValue());
+        Assertions.assertEquals(goal.getDueDate(), actualGoal.getDueDate());
+        Assertions.assertEquals(goal.getUser(), actualGoal.getUser());
     }
 
     @Test
