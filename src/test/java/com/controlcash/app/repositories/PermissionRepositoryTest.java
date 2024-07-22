edaafdc0cbb8a5d2008a14d9d931eff26bae847b
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-22 11:26:32 -0300

    test: Implementar teste para save de Permission com todos os atributos
    Issue: #26

--- src/test/java/com/controlcash/app/repositories/PermissionRepositoryTest.java
+++ src/test/java/com/controlcash/app/repositories/PermissionRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.controlcash.app.repositories;
 
+import com.controlcash.app.models.Permission;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,5 +16,20 @@ public class PermissionRepositoryTest {
     @Autowired
     private PermissionRepository permissionRepository;
 
+    private Permission permission;
 
+    @BeforeEach
+    void setUp() {
+        permission = new Permission();
+        permission.setDescription("Admin");
+    }
+
+    @Test
+    void testSave_GivenAPermissionWithAllAttributes_ShouldSaveAndReturnPermission() {
+        Permission actualPermission = permissionRepository.save(permission);
+
+        Assertions.assertNotNull(actualPermission);
+        Assertions.assertNotNull(actualPermission.getId());
+        Assertions.assertEquals(permission.getAuthority(), actualPermission.getAuthority());
+    }
 }
