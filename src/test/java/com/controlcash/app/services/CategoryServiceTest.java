4dfaadcc6d9dc2c08058b3ecbbc45e09f39be1f0
João Darwin <joaodarwin.ip22@gmail.com>
2024-07-30 22:23:09 -0300

    test: Implementar teste do método de create no service de Category
    Issue: #33

--- src/test/java/com/controlcash/app/services/CategoryServiceTest.java
+++ src/test/java/com/controlcash/app/services/CategoryServiceTest.java
@@ -1,11 +1,24 @@
 package com.controlcash.app.services;
 
+import com.controlcash.app.dtos.category.request.CategoryRequestDTO;
+import com.controlcash.app.dtos.category.response.CategoryResponseDTO;
+import com.controlcash.app.models.Category;
 import com.controlcash.app.repositories.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class CategoryServiceTest {
 
@@ -14,4 +27,23 @@ public class CategoryServiceTest {
 
     @InjectMocks
     private CategoryService categoryService;
+
+    private CategoryRequestDTO categoryRequestDTO;
+
+    @BeforeEach
+    void setUp() {
+        categoryRequestDTO = new CategoryRequestDTO("Electronics", List.of(), List.of());
+    }
+
+    @Test
+    void testCreate_GivenACategoryRequestDTO_ShouldReturnACategoryResponseDTO() {
+        Category expectedCategory = new Category(UUID.randomUUID(), "Electronics", List.of(), List.of());
+        Mockito.when(categoryRepository.save(Mockito.any())).thenReturn(expectedCategory);
+
+        CategoryResponseDTO actualCategory = categoryService.create(categoryRequestDTO);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertEquals(expectedCategory.getId(), actualCategory.id());
+        Assertions.assertEquals(expectedCategory.getName(), actualCategory.name());
+    }
 }
