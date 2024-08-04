0c4a9e4b68c6fa90f219e5c27c5eef8d8f56a5f9
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-03 23:29:34 -0300

    test: Implementar testes para método findAll de GoalService
    Issue: #34

--- src/test/java/com/controlcash/app/services/GoalServiceTest.java
+++ src/test/java/com/controlcash/app/services/GoalServiceTest.java
@@ -2,6 +2,7 @@ package com.controlcash.app.services;
 
 import com.controlcash.app.dtos.goal.request.GoalCreateRequestDTO;
 import com.controlcash.app.dtos.goal.response.GoalCompleteResponseDTO;
+import com.controlcash.app.dtos.goal.response.GoalSimpleResponseDTO;
 import com.controlcash.app.models.Category;
 import com.controlcash.app.models.Goal;
 import com.controlcash.app.models.User;
@@ -17,9 +18,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,13 +39,14 @@ public class GoalServiceTest {
     @InjectMocks
     private GoalService goalService;
 
+    private DateFormatUtils dateFormatUtils;
     private Goal goal;
     private GoalCreateRequestDTO goalCreateRequestDTO;
     private UUID id;
 
     @BeforeEach
     void setUp() throws ParseException {
-        DateFormatUtils dateFormatUtils = DateFormatUtils.getInstance();
+        dateFormatUtils = DateFormatUtils.getInstance();
         Date createdDate = dateFormatUtils.convertStringToDate("01/08/2024");
 
         id = UUID.randomUUID();
@@ -56,5 +64,31 @@ public class GoalServiceTest {
         Assertions.assertEquals(id, actualGoalCompleteResponseDTO.id());
         Assertions.assertEquals(goalCreateRequestDTO.dueDate(), actualGoalCompleteResponseDTO.dueDate());
         Assertions.assertEquals(goalCreateRequestDTO.value(), actualGoalCompleteResponseDTO.value());
+    }
+
+    @Test
+    void testFindAll_GivenAPageable_ShouldReturnAGoalSimpleResponseDTOPage() throws ParseException {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Direction.ASC, "dueDate"));
+        Goal goal2 = new Goal(UUID.randomUUID(), dateFormatUtils.convertStringToDate("01/09/2024"), 2400.0, new User(), new Category());
+        List<Goal> goalList = List.of(goal, goal2);
+        Page<Goal> goalPage = new PageImpl<>(goalList);
+        Mockito.when(goalRepository.findAll(Mockito.any(Pageable.class))).thenReturn(goalPage);
+
+        Page<GoalSimpleResponseDTO> actualGoalSimpleResponseDTOPage = goalService.findAll(pageable);
+
+        Assertions.assertNotNull(actualGoalSimpleResponseDTOPage);
+        Assertions.assertEquals(2, actualGoalSimpleResponseDTOPage.getSize());
+    }
+
+    @Test
+    void testFindAll_GivenAPageableWithNotExistentPageNumber_ShouldReturnAnEmptyPage() {
+        Pageable pageable = PageRequest.of(10, 10, Sort.by(Sort.Direction.ASC, "dueDate"));
+        Page<Goal> goalPage = Page.empty();
+        Mockito.when(goalRepository.findAll(Mockito.any(Pageable.class))).thenReturn(goalPage);
+
+        Page<GoalSimpleResponseDTO> actualGoalSimpleResponseDTOPage = goalService.findAll(pageable);
+
+        Assertions.assertNotNull(actualGoalSimpleResponseDTOPage);
+        Assertions.assertTrue(actualGoalSimpleResponseDTOPage.isEmpty());
     }
 }
