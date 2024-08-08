4fa3a302b9595b275b6862ef4ad86db25a97328a
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-07 22:43:57 -0300

    test: Adicionar testes para quando código passado na busca do id não for válido
    Issue: #40

--- src/test/java/com/controlcash/app/models/enums/TransactionTypeTest.java
+++ src/test/java/com/controlcash/app/models/enums/TransactionTypeTest.java
@@ -2,6 +2,7 @@ package com.controlcash.app.models.enums;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -18,5 +19,15 @@ public class TransactionTypeTest {
         TransactionType actualTransactionType = TransactionType.valueOf(code);
 
         Assertions.assertEquals(code, actualTransactionType.getCode());
+    }
+
+    @Test
+    void testValueOf_GivenANotValidCode_ShouldThrowsAnIllegalArgumentException() {
+        int code = 12;
+        String expectedMessageException = "Invalid TransactionType code. Used: " + code;
+
+        IllegalArgumentException actualException = Assertions.assertThrows(IllegalArgumentException.class, () -> TransactionType.valueOf(code));
+
+        Assertions.assertEquals(expectedMessageException, actualException.getMessage());
     }
 }
