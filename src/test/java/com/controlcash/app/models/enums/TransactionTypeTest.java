6e7faf44d2311ac85abaccc4e30d5fe5e2ef3754
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-07 22:30:19 -0300

    test: Implementar teste para busca de TransactionType por código
    Issue: #40

--- src/test/java/com/controlcash/app/models/enums/TransactionTypeTest.java
+++ src/test/java/com/controlcash/app/models/enums/TransactionTypeTest.java
@@ -1,8 +1,22 @@
 package com.controlcash.app.models.enums;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class TransactionTypeTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1",
+            "2"
+    })
+    void testValueOf_GivenValidCodes_ShouldReturnTheCorrespondingTransactionType(int code) {
+        TransactionType actualTransactionType = TransactionType.valueOf(code);
+
+        Assertions.assertEquals(code, actualTransactionType.getCode());
+    }
 }
