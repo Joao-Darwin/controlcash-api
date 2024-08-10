8c881c7564da566b63025f436a047d655d22fc85
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-10 19:06:02 -0300

    test: Adicionar mock para PasswordEncoder e verificação do método de encoder no teste de create
    Issue: #37

--- src/test/java/com/controlcash/app/services/UserServiceTest.java
+++ src/test/java/com/controlcash/app/services/UserServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,6 +22,9 @@ public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserService userService;
@@ -32,7 +36,8 @@ public class UserServiceTest {
     @BeforeEach
     void setUp() {
         id = UUID.randomUUID();
-        user = new User(id,
+        user = new User(
+                id,
                 "fooBar",
                 "foobar@gmail.com",
                 "123456",
@@ -45,15 +50,23 @@ public class UserServiceTest {
                 List.of(),
                 List.of(),
                 List.of());
-        userCreateRequestDTO = new UserCreateRequestDTO("fooBar", "foobar@gmail.com", "123456", "Foo Bar", 1500.00);
+        userCreateRequestDTO = new UserCreateRequestDTO(
+                "fooBar",
+                "foobar@gmail.com",
+                "123456",
+                "Foo Bar",
+                1500.00);
     }
 
     @Test
     void testCreate_GivenAnUserCreateRequestDTO_ShouldSaveAndReturnAnUserCreateResponseDTO() {
         Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        Mockito.when(passwordEncoder.encode(Mockito.anyString()))
+                .thenReturn("$2a$12$pQkRscbGtwUoX2bCqB6Ww.PTuDJlF/PnuosheRWv3jbL7nEicCc2u");
 
         UserCreateResponseDTO actualUserCreateResponseDTO = userService.create(userCreateRequestDTO);
 
+        Mockito.verify(passwordEncoder, Mockito.times(1)).encode(Mockito.anyString());
         Assertions.assertNotNull(actualUserCreateResponseDTO);
         Assertions.assertNotNull(actualUserCreateResponseDTO.id());
         Assertions.assertEquals(userCreateRequestDTO.email(), actualUserCreateResponseDTO.email());
