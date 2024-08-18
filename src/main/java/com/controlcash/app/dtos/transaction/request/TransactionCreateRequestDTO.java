5aedf15c9568f5db3fea8d087d1df26de7157480
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-17 23:23:15 -0300

    feat: Adicionar user no dto para criação de transaction
    Issue: #36

--- src/main/java/com/controlcash/app/dtos/transaction/request/TransactionCreateRequestDTO.java
+++ src/main/java/com/controlcash/app/dtos/transaction/request/TransactionCreateRequestDTO.java
@@ -1,9 +1,10 @@
 package com.controlcash.app.dtos.transaction.request;
 
 import com.controlcash.app.models.Category;
+import com.controlcash.app.models.User;
 import com.controlcash.app.models.enums.TransactionType;
 
 import java.util.List;
 
-public record TransactionCreateRequestDTO(String name, String description, Double value, Integer amountRepeat, TransactionType transactionType, List<Category> categories) {
+public record TransactionCreateRequestDTO(String name, String description, Double value, Integer amountRepeat, TransactionType transactionType, User user, List<Category> categories) {
 }
