37294fe353c1180b8d9159adb8ae66639d399357
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-17 22:34:17 -0300

    test: Implementar classe para criação de testes da entidade transaction
    Issue: #36

---
+++ src/test/java/com/controlcash/app/services/TransactionServiceTest.java
@@ -0,0 +1,23 @@
+package com.controlcash.app.services;
+
+import com.controlcash.app.repositories.TransactionRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class TransactionServiceTest {
+
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @InjectMocks
+    private TransactionService transactionService;
+
+    void setUp() {
+    }
+}
