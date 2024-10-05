a4a2820e07d097d3ea8073374d68570203f36722
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-04 22:43:10 -0300

    feat: Implementar endpoint de criação de Category
    Issue: #17

--- src/main/java/com/controlcash/app/controllers/category/ICategoryController.java
+++ src/main/java/com/controlcash/app/controllers/category/ICategoryController.java
@@ -1,4 +1,9 @@
 package com.controlcash.app.controllers.category;
 
+import com.controlcash.app.dtos.category.request.CategoryRequestDTO;
+import org.springframework.http.ResponseEntity;
+
 public interface ICategoryController {
+
+    ResponseEntity<?> create(CategoryRequestDTO categoryRequestDTO);
 }
--- src/main/java/com/controlcash/app/controllers/category/impl/CategoryController.java
+++ src/main/java/com/controlcash/app/controllers/category/impl/CategoryController.java
@@ -1,8 +1,13 @@
 package com.controlcash.app.controllers.category.impl;
 
 import com.controlcash.app.controllers.category.ICategoryController;
+import com.controlcash.app.dtos.category.request.CategoryRequestDTO;
+import com.controlcash.app.dtos.category.response.CategoryResponseDTO;
 import com.controlcash.app.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,16 @@ public class CategoryController implements ICategoryController {
     @Autowired
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(CategoryRequestDTO categoryRequestDTO) {
+        try {
+            CategoryResponseDTO categoryResponseDTO = categoryService.create(categoryRequestDTO);
+
+            return ResponseEntity.status(HttpStatus.OK).body(categoryResponseDTO);
+        } catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 }
