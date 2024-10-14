b0a8d97307bac2762e0e146429a289ca6563cdd7
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-13 23:34:20 -0300

    feat: Criar método de create na interface do controllador de goal
    Issue: #16

--- src/main/java/com/controlcash/app/controllers/goal/IGoalController.java
+++ src/main/java/com/controlcash/app/controllers/goal/IGoalController.java
@@ -1,4 +1,10 @@
 package com.controlcash.app.controllers.goal;
 
+import com.controlcash.app.dtos.goal.request.GoalCreateRequestDTO;
+import com.controlcash.app.dtos.goal.response.GoalCompleteResponseDTO;
+import org.springframework.http.ResponseEntity;
+
 public interface IGoalController {
+
+    ResponseEntity<GoalCompleteResponseDTO> create(GoalCreateRequestDTO goalCreateRequestDTO);
 }
--- src/main/java/com/controlcash/app/controllers/goal/impl/GoalController.java
+++ src/main/java/com/controlcash/app/controllers/goal/impl/GoalController.java
@@ -1,8 +1,14 @@
 package com.controlcash.app.controllers.goal.impl;
 
 import com.controlcash.app.controllers.goal.IGoalController;
+import com.controlcash.app.dtos.goal.request.GoalCreateRequestDTO;
+import com.controlcash.app.dtos.goal.response.GoalCompleteResponseDTO;
 import com.controlcash.app.services.GoalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +21,12 @@ public class GoalController implements IGoalController {
     @Autowired
     public GoalController(GoalService goalService) {
         this.goalService = goalService;
+    }
+
+    @PostMapping
+    public ResponseEntity<GoalCompleteResponseDTO> create(@RequestBody GoalCreateRequestDTO goalCreateRequestDTO) {
+        GoalCompleteResponseDTO goalCompleteResponseDTO = goalService.create(goalCreateRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.OK).body(goalCompleteResponseDTO);
     }
 }
