c7c33045bd08fa31f5f2fdb1c309b7abded9bf1c
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-13 23:57:55 -0300

    feat: Adicionar testes unitários para endpoint de findAll de Goal
    Issue: #16

--- src/test/java/com/controlcash/app/controllers/goal/impl/GoalControllerTest.java
+++ src/test/java/com/controlcash/app/controllers/goal/impl/GoalControllerTest.java
@@ -1,17 +1,37 @@
 package com.controlcash.app.controllers.goal.impl;
 
+import com.controlcash.app.dtos.goal.request.GoalCreateRequestDTO;
+import com.controlcash.app.dtos.goal.response.GoalCompleteResponseDTO;
+import com.controlcash.app.dtos.goal.response.GoalSimpleResponseDTO;
+import com.controlcash.app.models.Category;
+import com.controlcash.app.models.User;
 import com.controlcash.app.services.GoalService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@WebMvcTest(controllers = {GoalController.class})
 @AutoConfigureMockMvc(addFilters = false)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class GoalControllerTest {
@@ -26,7 +46,95 @@ public class GoalControllerTest {
     @MockBean
     private GoalService goalService;
 
+    private UUID id;
+    private GoalCreateRequestDTO goalCreateRequestDTO;
+    private GoalCompleteResponseDTO goalCompleteResponseDTO;
+
     @BeforeEach
     void setUp() {
+        User user = new User(
+                UUID.randomUUID(),
+                "foobar",
+                "foobar@gmail.com",
+                "123456",
+                "Foo Bar",
+                2500.00,
+                true,
+                true,
+                true,
+                true,
+                List.of(),
+                List.of(),
+                List.of());
+
+        Category category = new Category(UUID.randomUUID(), "Credit card", List.of(), List.of());
+
+        goalCreateRequestDTO = new GoalCreateRequestDTO(LocalDate.parse("2024-08-01"), 1200.0, user, category);
+        id = UUID.randomUUID();
+        goalCompleteResponseDTO = new GoalCompleteResponseDTO(id, LocalDate.parse("2024-08-01"), 1200.0, user, category);
+    }
+
+    @Test
+    void testCreate_GivenAGoalCreateRequestDTO_ShouldReturnAGoalCompleteResponseDTOAndOk() throws Exception {
+        Mockito.when(goalService.create(Mockito.any(GoalCreateRequestDTO.class))).thenReturn(goalCompleteResponseDTO);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post(GOAL_BASE_ENDPOINT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(goalCreateRequestDTO)));
+
+        Mockito.verify(goalService, Mockito.times(1)).create(Mockito.any());
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dueDate").value(goalCreateRequestDTO.dueDate()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value").value(goalCreateRequestDTO.value()));
+    }
+
+    @Test
+    void testFindAll_GivenAPageable_ShouldReturnAPageWithGoalSimpleResponseDTO() throws Exception {
+        GoalSimpleResponseDTO goalSimpleResponseDTO = new GoalSimpleResponseDTO(id, LocalDate.now(), 1500.0);
+        GoalSimpleResponseDTO goalSimpleResponseDTO2 = new GoalSimpleResponseDTO(id, LocalDate.now(), 1700.0);
+        Page<GoalSimpleResponseDTO> goalSimpleResponseDTOPage = new PageImpl<>(List.of(goalSimpleResponseDTO, goalSimpleResponseDTO2));
+        Mockito.when(goalService.findAll(Mockito.any(Pageable.class))).thenReturn(goalSimpleResponseDTOPage);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get(GOAL_BASE_ENDPOINT)
+                .queryParam("page", "0")
+                .queryParam("size", "5")
+                .queryParam("sort", "asc"));
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.first").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.last").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].value").value("1500.0"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].value").value("1700.0"));
+    }
+
+    @Test
+    void testFindAll_GivenRequestParamsToPaginationWithSortDesc_ShouldReturnAPageWithGoalSimpleResponseDTO() throws Exception {
+        GoalSimpleResponseDTO goalSimpleResponseDTO = new GoalSimpleResponseDTO(id, LocalDate.now(), 1700.0);
+        GoalSimpleResponseDTO goalSimpleResponseDTO2 = new GoalSimpleResponseDTO(id, LocalDate.now(), 1500.0);
+        Page<GoalSimpleResponseDTO> goalSimpleResponseDTOPage = new PageImpl<>(List.of(goalSimpleResponseDTO, goalSimpleResponseDTO2));
+        Mockito.when(goalService.findAll(Mockito.any(Pageable.class))).thenReturn(goalSimpleResponseDTOPage);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get(GOAL_BASE_ENDPOINT)
+                .queryParam("page", "0")
+                .queryParam("size", "5")
+                .queryParam("sort", "desc"));
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.first").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.last").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].value").value("1700.0"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].value").value("1500.0"));
     }
 }
