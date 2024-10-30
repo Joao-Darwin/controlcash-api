b9f71da1a677b119f42b5ea8f3b9d9216bedfb00
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-30 13:51:11 -0300

    feat: Criar endpoint de criação de Transaction
    Issue: #18

--- src/main/java/com/controlcash/app/controllers/transaction/impl/TransactionController.java
+++ src/main/java/com/controlcash/app/controllers/transaction/impl/TransactionController.java
@@ -1,8 +1,15 @@
 package com.controlcash.app.controllers.transaction.impl;
 
 import com.controlcash.app.controllers.transaction.ITransactionController;
+import com.controlcash.app.dtos.transaction.request.TransactionCreateRequestDTO;
+import com.controlcash.app.dtos.transaction.response.TransactionCreateResponseDTO;
 import com.controlcash.app.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +22,15 @@ class TransactionController implements ITransactionController {
     @Autowired
     TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
+    }
+
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    ResponseEntity<TransactionCreateResponseDTO> create(@RequestBody TransactionCreateRequestDTO transactionCreateRequestDTO) {
+        TransactionCreateResponseDTO transactionCreateResponseDTO = transactionService.create(transactionCreateRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.OK).body(transactionCreateResponseDTO);
     }
 }
