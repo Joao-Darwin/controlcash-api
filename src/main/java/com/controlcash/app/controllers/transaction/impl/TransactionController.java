fc3d73e31f5e8d7045d2cc61c0728860403855f2
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-31 22:19:39 -0300

    feat: Criar endpoints de findAll de transactions com paginação
    Issue: #18

--- src/main/java/com/controlcash/app/controllers/transaction/impl/TransactionController.java
+++ src/main/java/com/controlcash/app/controllers/transaction/impl/TransactionController.java
@@ -5,12 +5,18 @@ import com.controlcash.app.dtos.transaction.request.TransactionCreateRequestDTO;
 import com.controlcash.app.dtos.transaction.response.TransactionCreateResponseDTO;
 import com.controlcash.app.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("${path-api}/transactions")
@@ -32,5 +38,21 @@ class TransactionController implements ITransactionController {
         TransactionCreateResponseDTO transactionCreateResponseDTO = transactionService.create(transactionCreateRequestDTO);
 
         return ResponseEntity.status(HttpStatus.OK).body(transactionCreateResponseDTO);
+    }
+
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    ResponseEntity<Page<TransactionCreateResponseDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "asc") String sort) {
+
+        Sort.Direction sortDirection = sort.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "createdDate"));
+
+        Page<TransactionCreateResponseDTO> transactionCreateResponseDTOPage = transactionService.findAll(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(transactionCreateResponseDTOPage);
     }
 }
