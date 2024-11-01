4afd1d1baa9fc19eab9db97e62d3c5d91cd2395f
João Darwin <joaodarwin.ip22@gmail.com>
2024-10-31 22:18:57 -0300

    test: Criar testes para endpoint de findAll de transactions
    Issue: #18

--- src/test/java/com/controlcash/app/controllers/transaction/impl/TransactionControllerTest.java
+++ src/test/java/com/controlcash/app/controllers/transaction/impl/TransactionControllerTest.java
@@ -17,6 +17,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -102,5 +105,79 @@ public class TransactionControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.description").value(expectedDescription))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.createdDate").value(expectedCreatedDate.toString()));
 
+    }
+
+    @Test
+    void testFindAll_GivenAPageable_ShouldReturnAPageWithTransactionCreateResponseDTOAndOk() throws Exception {
+        UUID expectedId = UUID.randomUUID();
+        String expectedName = "Books";
+        String expectedDescription = "Books that i buy on Amazon";
+        LocalDate expectedCreatedDate = LocalDate.now();
+        TransactionCreateResponseDTO transactionCreateResponseDTO = new TransactionCreateResponseDTO(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedCreatedDate);
+        TransactionCreateResponseDTO transactionCreateResponseDTO2 = new TransactionCreateResponseDTO(
+                UUID.randomUUID(),
+                "Foo",
+                "Foo Bar",
+                LocalDate.now());
+        Page<TransactionCreateResponseDTO> expectedPage = new PageImpl<>(List.of(transactionCreateResponseDTO, transactionCreateResponseDTO2));
+        Mockito.when(transactionService.findAll(Mockito.any(Pageable.class))).thenReturn(expectedPage);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get(TRANSACTION_BASE_ENDPOINT)
+                .queryParam("page", "0")
+                .queryParam("size", "5")
+                .queryParam("sort", "asc"));
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.first").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.last").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id").value(expectedId.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value(expectedName))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].description").value(expectedDescription))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].createdDate").value(expectedCreatedDate.toString()));
+    }
+
+    @Test
+    void testFindAll_GivenAPageableWithDescSort_ShouldReturnAPageWithTransactionCreateResponseDTOAndOk() throws Exception {
+        UUID expectedId = UUID.randomUUID();
+        String expectedName = "Books";
+        String expectedDescription = "Books that i buy on Amazon";
+        LocalDate expectedCreatedDate = LocalDate.now();
+        TransactionCreateResponseDTO transactionCreateResponseDTO2 = new TransactionCreateResponseDTO(
+                expectedId,
+                expectedName,
+                expectedDescription,
+                expectedCreatedDate);
+        TransactionCreateResponseDTO transactionCreateResponseDTO = new TransactionCreateResponseDTO(
+                UUID.randomUUID(),
+                "Foo",
+                "Foo Bar",
+                LocalDate.now());
+        Page<TransactionCreateResponseDTO> expectedPage = new PageImpl<>(List.of(transactionCreateResponseDTO, transactionCreateResponseDTO2));
+        Mockito.when(transactionService.findAll(Mockito.any(Pageable.class))).thenReturn(expectedPage);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get(TRANSACTION_BASE_ENDPOINT)
+                .queryParam("page", "0")
+                .queryParam("size", "5")
+                .queryParam("sort", "desc"));
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.first").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.last").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id").value(expectedId.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value(expectedName))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].description").value(expectedDescription))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].createdDate").value(expectedCreatedDate.toString()));
     }
 }
