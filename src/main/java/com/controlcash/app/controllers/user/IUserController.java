0f209ea1476c8f0ad39cafb09553c4d7e11cc836
João Darwin <joaodarwin.ip22@gmail.com>
2024-12-10 16:12:49 -0300

    feat: Implementar endpoint de findAll de users
    Issue: #19

--- src/main/java/com/controlcash/app/controllers/user/IUserController.java
+++ src/main/java/com/controlcash/app/controllers/user/IUserController.java
@@ -1,9 +1,12 @@
 package com.controlcash.app.controllers.user;
 
 import com.controlcash.app.dtos.user.request.UserCreateRequestDTO;
+import com.controlcash.app.dtos.user.response.UserAllResponseDTO;
 import com.controlcash.app.dtos.user.response.UserCreateResponseDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 public interface IUserController {
     ResponseEntity<UserCreateResponseDTO> create(UserCreateRequestDTO userCreateRequestDTO);
+    ResponseEntity<Page<UserAllResponseDTO>> findAll(int page, int size, String sort);
 }
--- src/main/java/com/controlcash/app/controllers/user/impl/UserController.java
+++ src/main/java/com/controlcash/app/controllers/user/impl/UserController.java
@@ -2,15 +2,22 @@ package com.controlcash.app.controllers.user.impl;
 
 import com.controlcash.app.controllers.user.IUserController;
 import com.controlcash.app.dtos.user.request.UserCreateRequestDTO;
+import com.controlcash.app.dtos.user.response.UserAllResponseDTO;
 import com.controlcash.app.dtos.user.response.UserCreateResponseDTO;
 import com.controlcash.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +38,21 @@ public class UserController implements IUserController {
         UserCreateResponseDTO userCreateResponseDTO = userService.create(userCreateRequestDTO);
 
         return ResponseEntity.status(HttpStatus.OK).body(userCreateResponseDTO);
+    }
+
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Page<UserAllResponseDTO>> findAll(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "sort", defaultValue = "asc") String sort) {
+
+        Sort.Direction direction = sort.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, "username"));
+
+        Page<UserAllResponseDTO> userAllResponseDTOS = userService.findAll(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userAllResponseDTOS);
     }
 }
