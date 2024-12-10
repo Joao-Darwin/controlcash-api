0a85465a38035813007f98c67c0e8b5247649809
João Darwin <joaodarwin.ip22@gmail.com>
2024-12-10 16:11:28 -0300

    test: Implementar testes para endpoint de findAll de users
    Issue: #19

--- src/test/java/com/controlcash/app/controllers/user/impl/UserControllerTest.java
+++ src/test/java/com/controlcash/app/controllers/user/impl/UserControllerTest.java
@@ -1,9 +1,11 @@
 package com.controlcash.app.controllers.user.impl;
 
 import com.controlcash.app.dtos.user.request.UserCreateRequestDTO;
+import com.controlcash.app.dtos.user.response.UserAllResponseDTO;
 import com.controlcash.app.dtos.user.response.UserCreateResponseDTO;
 import com.controlcash.app.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -12,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +25,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
 import java.util.UUID;
 
 @WebMvcTest(controllers = UserController.class)
@@ -37,13 +43,22 @@ public class UserControllerTest {
     @MockBean
     private UserService userService;
 
+    private String expectedUsername;
+    private String expectedEmail;
+    private UUID expectedUUID;
+    private UserCreateResponseDTO userResponse;
+
+    @BeforeEach
+    void setUp() {
+        expectedUsername = "foobar";
+        expectedEmail = "foobar@gmail.com";
+        expectedUUID = UUID.randomUUID();
+        userResponse = new UserCreateResponseDTO(expectedUUID, expectedUsername, expectedEmail);
+    }
+
     @Test
     void testCreate_GivenAValidUserCreateRequestDTO_ShouldReturnAnUserCreateResponseDTOAndOk() throws Exception {
-        String expectedUsername = "foobar";
-        String expectedEmail = "foobar@gmail.com";
-        UUID expectedUUID = UUID.randomUUID();
         UserCreateRequestDTO userRequest = new UserCreateRequestDTO(expectedUsername, expectedEmail, "123456", "Foo Bar", 1405.00);
-        UserCreateResponseDTO userResponse = new UserCreateResponseDTO(expectedUUID, expectedUsername, expectedEmail);
         Mockito.when(userService.create(Mockito.any(UserCreateRequestDTO.class))).thenReturn(userResponse);
 
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post(USER_BASE_ENDPOINT)
@@ -56,5 +71,59 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(expectedUUID.toString()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.userName").value(expectedUsername))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(expectedEmail));
+
+        Mockito.verify(userService, Mockito.times(1)).create(Mockito.any(UserCreateRequestDTO.class));
+    }
+
+    @Test
+    void testFindAll_GivenAnAscPageable_ShouldReturnAPageWithUserCreateResponseDTOAndOk() throws Exception {
+        UserAllResponseDTO userAllResponseDTO = new UserAllResponseDTO(expectedUUID, expectedUsername, expectedEmail);
+        UserAllResponseDTO userAllResponseDTO2 = new UserAllResponseDTO(UUID.randomUUID(), "foobar2", "foobar2@gmail.com");
+        Page<UserAllResponseDTO> pageWithUsers = new PageImpl<>(List.of(userAllResponseDTO, userAllResponseDTO2));
+        Mockito.when(userService.findAll(Mockito.any(Pageable.class))).thenReturn(pageWithUsers);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get(USER_BASE_ENDPOINT)
+                .queryParam("page", "0")
+                .queryParam("size", "5")
+                .queryParam("sort", "asc"));
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.first").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.last").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id").value(expectedUUID.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].userName").value(expectedUsername))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].email").value(expectedEmail));
+
+        Mockito.verify(userService, Mockito.times(1)).findAll(Mockito.any(Pageable.class));
+    }
+
+    @Test
+    void testFindAll_GivenAnDescPageable_ShouldReturnAPageWithUserCreateResponseDTOAndOk() throws Exception {
+        UserAllResponseDTO userAllResponseDTO = new UserAllResponseDTO(UUID.randomUUID(), "foobar2", "foobar2@gmail.com");
+        UserAllResponseDTO userAllResponseDTO2 = new UserAllResponseDTO(expectedUUID, expectedUsername, expectedEmail);
+        Page<UserAllResponseDTO> pageWithUsers = new PageImpl<>(List.of(userAllResponseDTO, userAllResponseDTO2));
+        Mockito.when(userService.findAll(Mockito.any(Pageable.class))).thenReturn(pageWithUsers);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get(USER_BASE_ENDPOINT)
+                .queryParam("page", "0")
+                .queryParam("size", "5")
+                .queryParam("sort", "desc"));
+
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalPages").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.first").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.last").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id").value(expectedUUID.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].userName").value(expectedUsername))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].email").value(expectedEmail));
+
+        Mockito.verify(userService, Mockito.times(1)).findAll(Mockito.any(Pageable.class));
     }
 }
