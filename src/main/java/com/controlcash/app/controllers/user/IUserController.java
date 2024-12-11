1f8fd52ae4d62039b1a6dcc7ddf567d3c1fbaa00
João Darwin <joaodarwin.ip22@gmail.com>
2024-12-11 09:55:29 -0300

    feat: Implementar endpoint de findById para users
    Issue: #19

--- src/main/java/com/controlcash/app/controllers/user/IUserController.java
+++ src/main/java/com/controlcash/app/controllers/user/IUserController.java
@@ -6,7 +6,10 @@ import com.controlcash.app.dtos.user.response.UserCreateResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
+import java.util.UUID;
+
 public interface IUserController {
     ResponseEntity<UserCreateResponseDTO> create(UserCreateRequestDTO userCreateRequestDTO);
     ResponseEntity<Page<UserAllResponseDTO>> findAll(int page, int size, String sort);
+    ResponseEntity<?> findById(UUID id);
 }
--- src/main/java/com/controlcash/app/controllers/user/impl/UserController.java
+++ src/main/java/com/controlcash/app/controllers/user/impl/UserController.java
@@ -3,7 +3,10 @@ package com.controlcash.app.controllers.user.impl;
 import com.controlcash.app.controllers.user.IUserController;
 import com.controlcash.app.dtos.user.request.UserCreateRequestDTO;
 import com.controlcash.app.dtos.user.response.UserAllResponseDTO;
+import com.controlcash.app.dtos.user.response.UserCompleteResponseDTO;
 import com.controlcash.app.dtos.user.response.UserCreateResponseDTO;
+import com.controlcash.app.exceptions.ResponseEntityException;
+import com.controlcash.app.exceptions.UserNotFoundException;
 import com.controlcash.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,11 +17,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("${path-api}/users")
@@ -54,5 +61,21 @@ public class UserController implements IUserController {
         Page<UserAllResponseDTO> userAllResponseDTOS = userService.findAll(pageable);
 
         return ResponseEntity.status(HttpStatus.OK).body(userAllResponseDTOS);
+    }
+
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<?> findById(@PathVariable(name = "id") UUID id) {
+        try {
+            UserCompleteResponseDTO user = userService.findById(id);
+
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        } catch (UserNotFoundException userNotFoundException) {
+            ResponseEntityException responseEntityException = new ResponseEntityException(Instant.now(), userNotFoundException.getMessage(), "");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseEntityException);
+        }
     }
 }
