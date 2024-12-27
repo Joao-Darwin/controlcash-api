69177e055f8407fb56667afd4642151ba0a4ca49
João Darwin <joaodarwin.ip22@gmail.com>
2024-12-27 11:08:23 -0300

    feat: Criar endpoint de criação de permission

--- src/main/java/com/controlcash/app/controllers/permission/impl/PermissionController.java
+++ src/main/java/com/controlcash/app/controllers/permission/impl/PermissionController.java
@@ -1,8 +1,15 @@
 package com.controlcash.app.controllers.permission.impl;
 
 import com.controlcash.app.controllers.permission.IPermissionController;
+import com.controlcash.app.dtos.permission.request.PermissionCreateRequestDTO;
+import com.controlcash.app.dtos.permission.response.PermissionResponseDTO;
 import com.controlcash.app.services.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +21,15 @@ public class PermissionController implements IPermissionController {
     @Autowired
     public PermissionController(PermissionService permissionService) {
         this.permissionService = permissionService;
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<PermissionResponseDTO> create(@RequestBody PermissionCreateRequestDTO permission) {
+        PermissionResponseDTO permissionResponseDTO = permissionService.create(permission);
+
+        return ResponseEntity.status(HttpStatus.OK).body(permissionResponseDTO);
     }
 }
