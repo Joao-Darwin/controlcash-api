82e5562ca0d623a4bbc295f5bcd664bf9b008d8a
João Darwin <joaodarwin.ip22@gmail.com>
2024-12-27 11:16:37 -0300

    feat: Adicionar tratamento de exceção quando argumento não for válido
    Issue: #20

--- src/main/java/com/controlcash/app/controllers/permission/IPermissionController.java
+++ src/main/java/com/controlcash/app/controllers/permission/IPermissionController.java
@@ -1,9 +1,8 @@
 package com.controlcash.app.controllers.permission;
 
 import com.controlcash.app.dtos.permission.request.PermissionCreateRequestDTO;
-import com.controlcash.app.dtos.permission.response.PermissionResponseDTO;
 import org.springframework.http.ResponseEntity;
 
 public interface IPermissionController {
-    ResponseEntity<PermissionResponseDTO> create(PermissionCreateRequestDTO permission);
+    ResponseEntity<?> create(PermissionCreateRequestDTO permission);
 }
--- src/main/java/com/controlcash/app/controllers/permission/impl/PermissionController.java
+++ src/main/java/com/controlcash/app/controllers/permission/impl/PermissionController.java
@@ -3,6 +3,7 @@ package com.controlcash.app.controllers.permission.impl;
 import com.controlcash.app.controllers.permission.IPermissionController;
 import com.controlcash.app.dtos.permission.request.PermissionCreateRequestDTO;
 import com.controlcash.app.dtos.permission.response.PermissionResponseDTO;
+import com.controlcash.app.exceptions.ResponseEntityException;
 import com.controlcash.app.services.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @RestController
 @RequestMapping("${path-api}/permissions")
@@ -27,9 +30,18 @@ public class PermissionController implements IPermissionController {
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
-    public ResponseEntity<PermissionResponseDTO> create(@RequestBody PermissionCreateRequestDTO permission) {
-        PermissionResponseDTO permissionResponseDTO = permissionService.create(permission);
+    public ResponseEntity<?> create(@RequestBody PermissionCreateRequestDTO permission) {
+        try {
+            PermissionResponseDTO permissionResponseDTO = permissionService.create(permission);
 
-        return ResponseEntity.status(HttpStatus.OK).body(permissionResponseDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(permissionResponseDTO);
+        } catch (IllegalArgumentException argumentException) {
+            ResponseEntityException response = new ResponseEntityException(
+                    Instant.now(),
+                    argumentException.getMessage(),
+                    "uri=/api/permissions"
+            );
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 }
